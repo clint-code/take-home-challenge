5f2fa6fb2bf55b748f939d7997af654571badd4b
Clinton Mutinda <mutinda1993@gmail.com>
2024-06-15 20:20:11 +0300

    Getting access to the GraphQL API in the GetBooks component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,86 +3,50 @@ import React, { Component } from 'react';
 import './App.css';
 
 import { Button, Box, Autocomplete, TextField, ImageList, ImageListItem, ImageListItemBar } from '@mui/material';
+import GetBooks from './GetBooks';
+
+import { 
+  ApolloClient, 
+  InMemoryCache, 
+  ApolloProvider,
+  HttpLink,
+  from} 
+  from '@apollo/client';
+
+import{onError} from '@apollo/client/link/error';
+
+const errorLink = onError(({ graphqlErrors, networkError}) => {
+  if (graphqlErrors) {
+      graphqlErrors.map(({message, location, path}) => {
+        alert(`Graphql error ${message}`);
+      });
+  }
+});
+
+// const link = from({
+//   errorLink,
+//   new HttpLink({uri: "http://localhost:4000/"}),
+// });
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000/",
+  errorLink
+})
+
+const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink
+})
 
 const App = () => {
   
-  const books = ['Option 1', 'Option 2', 'Option 3'];
-
-  const bookData = [
-    {
-      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-      title: 'Breakfast',
-      author: '@bkristastucchio',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-      title: 'Burger',
-      author: '@rollelflex_graphy726',
-    },
-    {
-      img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-      title: 'Camera',
-      author: '@helloimnik',
-    }
-  ]
 
   return (
 
-    <div className="App">
+    <ApolloProvider client={client}>
+        <GetBooks />
+    </ApolloProvider>
 
-      <Box component="section" sx={{ p:2 }}>
-
-        <Autocomplete
-          options={books}
-          autoHighlight
-          renderOption={(props, option) => (
-            <Box component="li" sx={{ '& > img': {mr:2, flexShrink: 0 } }} {...props}>
-              <img
-                loading="lazy"
-                width="50"
-                srcSet={`https://i.pinimg.com/564x/ba/7a/ac/ba7aacb6de87e3646f58bc35632ab02d.jpg`}
-                src={`https://i.pinimg.com/564x/ba/7a/ac/ba7aacb6de87e3646f58bc35632ab02d.jpg`}
-                alt=""
-              />
-              {books}
-            </Box>
-          )}
-          renderInput={(params) => <TextField {...params} />}
-        />
-
-          <Box component="section">
-
-            <ImageList sx={{width:650}}>
-              {bookData.map((item) => (
-                <ImageListItem key={item.img}>
-                  <img
-                    srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                    src={`${item.img}?w=248&fit=crop&auto=format`}
-                    alt={item.title}
-                    loading="lazy"    
-                  />
-                  <ImageListItemBar
-                    title={item.title}
-                    subtitle={<span>by: {item.author}</span>}
-                    position="below"
-                  />
-
-                  <Button color="success">
-                    Add to Reading List
-                  </Button>
-
-                </ImageListItem>
-              ))}
-
-            </ImageList>
-
-
-          </Box>
-
-
-      </Box>
-
-    </div>
   );
 
 }
