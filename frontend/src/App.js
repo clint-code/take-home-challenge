6cee59538ee3ab3b9ac661fb455968d075047b2c
Clinton Mutinda <mutinda1993@gmail.com>
2024-06-15 13:58:54 +0300

    Set up react project in frontend folder, installed material-ui and added Mulish Google font

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+import Button from '@mui/material/Button';
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import RemoveCircleOutlineIcon from '@mui/icons-material/RemoveCircleOutline';
+import { Autocomplete } from '@mui/material';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>It works</h1>
+    </div>
+  );
+}
+
+export default App;
